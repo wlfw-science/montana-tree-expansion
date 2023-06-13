9fc62120863a26b1ea51eda8c2d3260699469830
jmalczyk <jmalczyk@gmail.com>
2023-06-13 17:41:38 -0400

    Updates base href for custom URL.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-ng build --base-href "http://montana-app.landscapeexplorer.org/"
+ng build --base-href "https://montana-app.landscapeexplorer.org/"
 cp ./dist/index.html ./dist/404.html
 cp ./dist/index.html ./dist/200.html
 /usr/local/bin/ngh:
