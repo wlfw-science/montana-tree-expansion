e0336c7fc9bef72743a96da4ed75ba9c5ce5269c
jmalczyk <jmalczyk@gmail.com>
2023-06-19 17:14:40 -0400

    Deploying with production URL

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-# ng build --base-href "https://app.landscapeexplorer.org/"
-ng build --base-href "https://wlfw-science.github.io/landscape-explorer/"
+ng build --base-href "https://app.landscapeexplorer.org/"
+# ng build --base-href "https://wlfw-science.github.io/landscape-explorer/"
 cp ./dist/index.html ./dist/404.html
 cp ./dist/index.html ./dist/200.html
 /usr/local/bin/ngh:
